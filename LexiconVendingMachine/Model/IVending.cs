3689ccc5663b11c0ea9c73625ab9a27f90c4e256
GumUlf <programming@gumulf.se>
2021-10-24 20:29:57 +0200

    Interface for vending machines

---
+++ LexiconVendingMachine/Model/IVending.cs
@@ -0,0 +1,15 @@
+﻿namespace LexiconVendingMachine.Model
+{
+	interface IVending
+	{
+		Product[] ShowAll();
+
+		int InsertMoney(int money);
+
+		bool Purchase(out Product product, out bool endTransaction, out bool needMoreMoney);
+
+		int EndTransaction();
+
+
+	}
+}
