8fd28de8c5d53f89b73135dc20003d707cc531ba
GumUlf <programming@gumulf.se>
2021-10-24 21:22:05 +0200

    Starting tests of Inventory

---
+++ LexiconVendingMachine.Tests/Data/InventoryShould.cs
@@ -0,0 +1,25 @@
+﻿using LexiconVendingMachine.Data;
+using System;
+using Xunit;
+
+
+namespace LexiconVendingMachine.Tests.Data
+{
+	public class InventoryShould
+	{
+		[Fact]
+		public void BeEmptyOnCreation()
+		{
+			// Act
+
+			Inventory sut = new Inventory();
+
+			// Assert
+
+			Assert.Equal(0, sut.Size);
+			Assert.Empty(sut.GetAll());
+
+
+		}
+	}
+}
