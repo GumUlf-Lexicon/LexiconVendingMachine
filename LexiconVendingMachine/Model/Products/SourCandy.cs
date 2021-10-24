fc16012fb8900fad10b2c4240c54f4f0ade3f827
GumUlf <programming@gumulf.se>
2021-10-24 22:27:00 +0200

    Added some basic products

---
+++ LexiconVendingMachine/Model/Products/SourCandy.cs
@@ -0,0 +1,19 @@
+﻿namespace LexiconVendingMachine.Model.Products
+{
+	class SourCandy: Product
+	{
+
+		public SourCandy() : this(16) { }
+
+		public SourCandy(int price)
+		{
+			Price = price;
+			Description = "A bag of really sour candy.";
+		}
+
+		public override void Use()
+		{
+			System.Console.WriteLine("You open the bag and tastes one of the candies. Your face immediately makes an impression of a rasin.");
+		}
+	}
+}
---
+++ LexiconVendingMachine/Model/Products/SweetFizzyDrink.cs
@@ -0,0 +1,20 @@
+﻿namespace LexiconVendingMachine.Model.Products
+{
+	class SweetFizzyDrink: Product
+	{
+
+		public SweetFizzyDrink() : this(32) { }
+
+		public SweetFizzyDrink(int price)
+		{
+			Price = price;
+			Description = "A sweet sparkling drink tasting of cloudberries.";
+		}
+
+		public override void Use()
+		{
+			System.Console.WriteLine("You open the drink and take a sip of it. You get an energy boost and runs around fast like a rabbit.");
+		}
+
+	}
+}
